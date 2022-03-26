ce3c5cf7c0b186d4e61a4f2a888e719ff6cf6dbf
wshangold24 <89747248+wshangold24@users.noreply.github.com>
2022-03-25 19:42:44 -0500

    added scroll fade to rest of pages

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // Fade in code source: https://stackoverflow.com/questions/26694385/fade-in-on-scroll-down-fade-out-on-scroll-up-based-on-element-position-in-win
 
 
-$(window).on("load",function() {
-    function fade() {
-        var animation_height = $(window).innerHeight() * 0.25;
-        var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
+// $(window).on("load",function() {
+//     function fade() {
+//         var animation_height = $(window).innerHeight() * 0.25;
+//         var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
 
-        $('.fade').each(function() {
+//         $('.fade').each(function() {
             
-            var objectTop = $(this).offset().top;
-            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+//             var objectTop = $(this).offset().top;
+//             var windowBottom = $(window).scrollTop() + $(window).innerHeight();
             
-            if ( objectTop < windowBottom ) {
-                if ( objectTop < windowBottom - animation_height ) {
-                    $(this).html( 'fully visible' );
-                    $(this).css( {
-                        transition: 'opacity 0.1s linear',
-                        opacity: 1
-                    } );
+//             if ( objectTop < windowBottom ) {
+//                 if ( objectTop < windowBottom - animation_height ) {
+//                     $(this).html( 'fully visible' );
+//                     $(this).css( {
+//                         transition: 'opacity 0.1s linear',
+//                         opacity: 1
+//                     } );
 
-                } else {
-                    $(this).html( 'fading in/out' );
-                    $(this).css( {
-                        transition: 'opacity 0.25s linear',
-                        opacity: (windowBottom - objectTop) * ratio
-                    } );
-                }
-            } else {
-                $(this).html( 'not visible' );
-                $(this).css( 'opacity', 0 );
-            }
-        });
-    }
-    $('.fade').css( 'opacity', 0 );
-    fade();
-    $(window).scroll(function() {fade();});
-});
+//                 } else {
+//                     $(this).html( 'fading in/out' );
+//                     $(this).css( {
+//                         transition: 'opacity 0.25s linear',
+//                         opacity: (windowBottom - objectTop) * ratio
+//                     } );
+//                 }
+//             } else {
+//                 $(this).html( 'not visible' );
+//                 $(this).css( 'opacity', 0 );
+//             }
+//         });
+//     }
+//     $('.fade').css( 'opacity', 0 );
+//     fade();
+//     $(window).scroll(function() {fade();});
+// });
